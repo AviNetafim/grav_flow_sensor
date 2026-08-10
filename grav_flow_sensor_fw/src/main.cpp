00340b4811f591971176315cf5fe0311effa0532
Avi Schweitzer | Orbia (Netafim) <Avi.Schweitzer@netafim.com>
2026-08-10 15:15:33 +0300

    debuging weight conversion take f

--- grav_flow_sensor_fw/src/main.cpp
+++ grav_flow_sensor_fw/src/main.cpp
@@ -208,18 +208,19 @@ static void weight_test_task(void *arg){
             continue;
         }
 
-        const int64_t tare_raw = static_cast<int64_t>(adc_sample.raw);                            // update tare value (raw)
+        const int64_t tare_raw = static_cast<int64_t>(adc_sample.raw);      // update tare value (raw)
         ESP_LOGI(TAG, "Tare raw = %" PRId64 "" , tare_raw);
         const int64_t target_raw_0 = static_cast<int64_t>(-CAL_OFFSET * CAL_DIV / CAL_MUL);
         ESP_LOGI(TAG, "target 0 = %" PRId64 "" , target_raw_0);
-        const int64_t target_delta_raw =                                    // target raw = target_10mg 
+        const int64_t target_delta_raw_abs =                                    // target raw = target_10mg 
             static_cast<int64_t>(target_weight_10mg - CAL_OFFSET) * CAL_DIV / CAL_MUL;  // targert weigth converted to raw  units
+        const int64_t target_delta_raw = target_delta_raw_abs - target_tare_0;
         ESP_LOGI(TAG, "target delta = %" PRId64 "" , target_delta_raw);
-            const int64_t started_us = esp_timer_get_time();                    // start_us (since timer was initialized)
-        int64_t last_weight_print_us = started_us;                            // TEMP DEBUG
+            const int64_t started_us = esp_timer_get_time();                // start_us (since timer was initialized)
+        int64_t last_weight_print_us = started_us;                          // TEMP DEBUG
         TickType_t next_wake = xTaskGetTickCount();                         // rtos ticks since task was scheduled
 
-        ESP_LOGI(TAG, "Weight test started: target=%" PRId64 " x 10 mg, timeout=%u ds",
+        ESP_LOGI(TAG, "Weight test started: target = %u x 10 mg, timeout=%u ds",
                  target_weight_10mg, timeout_ds);
 
         while (true) {
